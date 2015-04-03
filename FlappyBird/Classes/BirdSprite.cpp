a59d98c7259a2939befb9978ebde15dae2edde3d
Allen Kashiwa <flyingcoding@gmail.com>
2015-04-03 23:13:16 +0800

    add birdsprite

---
+++ FlappyBird/Classes/BirdSprite.cpp
@@ -0,0 +1,9 @@
+//
+//  BirdSprite.cpp
+//  FlappyBird
+//
+//  Created by baizihan on 4/3/15.
+//
+//
+
+#include "BirdSprite.h"
---
+++ FlappyBird/Classes/BirdSprite.h
@@ -0,0 +1,14 @@
+//
+//  BirdSprite.h
+//  FlappyBird
+//
+//  Created by baizihan on 4/3/15.
+//
+//
+
+#ifndef __FlappyBird__BirdSprite__
+#define __FlappyBird__BirdSprite__
+
+#include <stdio.h>
+
+#endif /* defined(__FlappyBird__BirdSprite__) */
