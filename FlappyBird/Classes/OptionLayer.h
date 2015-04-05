125169c34f6e261a664686d7b25d6cfe3805b7b8
Allen Kashiwa <flyingcoding@gmail.com>
2015-04-05 21:43:40 +0800

    OptionLayer implemention

--- FlappyBird/Classes/OptionLayer.h
+++ FlappyBird/Classes/OptionLayer.h
@@ -9,6 +9,25 @@
 #ifndef __FlappyBird__OptionLayer__
 #define __FlappyBird__OptionLayer__
 
-#include <stdio.h>
+#include "cocos2d.h"
+
+using namespace cocos2d;
+
+class OptionDelegate{
+public:
+    virtual void onTouch() = 0;
+};
+
+class OptionLayer : public Layer {
+    
+public:
+    OptionLayer();
+    ~OptionLayer();
+    virtual bool init();
+    CREATE_FUNC(OptionLayer);
+    //override
+    void onTouchesBegan(const std::vector<Touch*> &touches,Event *event);
+    CC_SYNTHESIZE(OptionDelegate*, delegator, Delegator);
+};
 
 #endif /* defined(__FlappyBird__OptionLayer__) */
