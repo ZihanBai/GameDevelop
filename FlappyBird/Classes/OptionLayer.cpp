3b83b1424c6c1a80298aad4536ff1e1b2724a378
Allen Kashiwa <flyingcoding@gmail.com>
2015-04-05 21:33:33 +0800

    add optionLayer

---
+++ FlappyBird/Classes/OptionLayer.cpp
@@ -0,0 +1,9 @@
+//
+//  OptionLayer.cpp
+//  FlappyBird
+//
+//  Created by baizihan on 4/5/15.
+//
+//
+
+#include "OptionLayer.h"
---
+++ FlappyBird/Classes/OptionLayer.h
@@ -0,0 +1,14 @@
+//
+//  OptionLayer.h
+//  FlappyBird
+//
+//  Created by baizihan on 4/5/15.
+//
+//
+
+#ifndef __FlappyBird__OptionLayer__
+#define __FlappyBird__OptionLayer__
+
+#include <stdio.h>
+
+#endif /* defined(__FlappyBird__OptionLayer__) */
