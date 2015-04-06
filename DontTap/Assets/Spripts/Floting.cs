21cfd475361801163efc5faa5e25684e9f5788a9
Allen Kashiwa <flyingcoding@gmail.com>
2015-04-06 14:17:56 +0800

    Add a new Project
    Add a new Unity2d game project named DontTap.And I have already ignore
    the files which are created by Unity automatically.

---
+++ DontTap/Assets/Spripts/Floting.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Floting : MonoBehaviour {
+
+	private float redian = 0;
+	public float perRedian = 0.02f;
+	public float radius = 0.2f;
+
+	private Vector3 oldPos;
+
+	// Use this for initialization
+	void Start () {
+		oldPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		redian += perRedian;
+		float dy = Mathf.Cos (redian) * radius;
+		transform.position = oldPos + new Vector3 (0, dy, 0);
+	}
+}
