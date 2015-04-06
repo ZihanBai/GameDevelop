77e8f3cc0d6644ff84065b1e15d455d675889bf7
Allen Kashiwa <flyingcoding@gmail.com>
2015-04-06 15:58:42 +0800

    add block prefab
    add white  and normal block prefabs.And init block when game start in
    gameController.

---
+++ DontTap/Assets/Spripts/Block.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+
+	private float xOff = -2.1f;
+	private float yOff = -3.6f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnMouseDown () {
+		print ("Block onMouseDown");
+//		int colIndex;
+//		colIndex = Random.Range (0, 4);
+//		SetPosition (colIndex,0);
+	}
+
+	public void SetPosition(int colIndex,int rowIndex){
+		gameObject.transform.position = new Vector3 (xOff + colIndex * 1.5f, yOff + rowIndex * 2.8f);
+		
+	}
+}
--- DontTap/Assets/Spripts/GameController.cs
+++ DontTap/Assets/Spripts/GameController.cs
@@ -4,16 +4,29 @@ using System.Collections;
 public class GameController : MonoBehaviour {
 	public GameObject startUI;
 	public GameObject gameUI;
-
+	public GameObject container;
+	public GameObject whiteBlock;
+	public GameObject normalBlock;
 	//goto GameUI
 	public void EnterGameUI(){
 		startUI.SetActive (false);
 		gameUI.SetActive (true);
+		StartGame ();
 	}
 
 	//return StartUI
 	public void ReturnStartUI(){
 		startUI.SetActive (true);
 		gameUI.SetActive (false);
+	}
+
+	private void StartGame(){
+		for (int rowIndex = 0; rowIndex < 4; rowIndex++) {
+			GameObject o = Instantiate(whiteBlock) as GameObject;
+			o.transform.parent = container.transform;
+			Block b = o.GetComponent<Block>();
+			int colIndex = Random.Range (0, 4);
+			b.SetPosition(colIndex,rowIndex);
+		}
 	}
 }
