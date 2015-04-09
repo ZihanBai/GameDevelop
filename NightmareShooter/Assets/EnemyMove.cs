9db9bab82319648c6c75b22908b9dd86f20b8544
Allen Kashiwa <flyingcoding@gmail.com>
2015-04-09 14:23:10 +0800

    添加Enemy追击主角运动

---
+++ NightmareShooter/Assets/EnemyMove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMove : MonoBehaviour {
+
+	private NavMeshAgent agent;
+	private Transform player;
+
+	void Awake(){
+		agent = this.GetComponent<NavMeshAgent> ();
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag (Tags.player).transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		agent.SetDestination(player.position);
+
+	}
+}
