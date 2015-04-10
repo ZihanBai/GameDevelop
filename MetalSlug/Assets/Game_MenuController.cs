47170b26a96da6b03e34a97a082f1c3e7d92a336
Allen Kashiwa <flyingcoding@gmail.com>
2015-04-10 17:11:52 +0800

    完成游戏菜单跳转逻辑

--- MetalSlug/Assets/Game_MenuController.cs
+++ MetalSlug/Assets/Game_MenuController.cs
@@ -3,13 +3,41 @@ using System.Collections;
 
 public class Game_MenuController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public GameObject menuPanel;
+	public GameObject operationPanel;
+	public GameObject diffcultyPanel;
+	public GameObject charactorSelectPanel;
+	public Color operationPanelColor;
+
+	/// <summary>
+	/// Raises the menu play event.
+	/// </summary>
+	public void OnMenuPlay(){
+		menuPanel.SetActive (false);
+		operationPanel.SetActive (true);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	/// <summary>
+	/// Raises the operation play event.
+	/// </summary>
+	public void OnOperationPlay(){
+		operationPanel.GetComponent<UISprite> ().color = operationPanelColor;
+		diffcultyPanel.SetActive (true);
+	}
+
+	/// <summary>
+	/// Select Diffcult.
+	/// </summary>
+	public void OnDiffcultyClick(){
+		operationPanel.SetActive (false);
+		diffcultyPanel.SetActive (false);
+		charactorSelectPanel.SetActive (true);
+	}
+
+	/// <summary>
+	/// Raises the charactor select event.
+	/// </summary>
+	public void OnCharactorSelect(){
+		Application.LoadLevel (1);
 	}
 }
