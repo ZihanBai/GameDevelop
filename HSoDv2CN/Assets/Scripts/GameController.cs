bc87b14a6ab907fc67e0f8e1e20247a5b74a35b1
Allen Kashiwa <flyingcoding@gmail.com>
2015-04-13 17:52:48 +0800

    完成场景1，创建场景2，完成场景跳转

---
+++ HSoDv2CN/Assets/Scripts/GameController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	/// <summary>
+	/// Raises the screen tap event.
+	/// </summary>
+	public void OnScreenTap(){
+		print("test");
+		Application.LoadLevel (1);
+	}
+}
---
+++ HSoDv2CN/Assets/Scripts/ShowTip.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowTip : MonoBehaviour {
+
+	private float timer = 0f;
+	
+	private float showRate = 1f;
+	
+	private float showInterval = 0f;
+	
+	private bool isShowing = false;
+
+	public GameObject Tip;
+
+	// Use this for initialization
+	void Start () {
+		showInterval = 1 / showRate;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		Tip.gameObject.SetActive (true);
+		timer += Time.deltaTime;
+		if (timer > showInterval) {
+			Tip.gameObject.SetActive (isShowing);
+			isShowing = !isShowing;
+			timer -= showInterval;
+		}
+	}
+}
