85fea742ecb46cc8cd4683c4b2e2449c0fb78afd
Allen Kashiwa <flyingcoding@gmail.com>
2015-04-14 21:20:14 +0800

    控制角色移动

--- HSoDv2CN/Assets/Scripts/PlayerMove.cs
+++ HSoDv2CN/Assets/Scripts/PlayerMove.cs
@@ -3,13 +3,35 @@ using System.Collections;
 
 public class PlayerMove : MonoBehaviour {
 
+	public float speed = 5f;
+
+	private Rigidbody myRigidBody;
+
 	// Use this for initialization
 	void Start () {
-	
+		myRigidBody = this.GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//handle Left (A) and Right (D) button
+		HandleHorizontalBtn ();
+		HandleVerticalBtn ();
 	
+	}
+
+	/// <summary>
+	/// Handles the horizontal button.
+	/// </summary>
+	private void HandleHorizontalBtn(){
+		float h = Input.GetAxis ("Horizontal");
+		Vector3 r = myRigidBody.velocity;
+		myRigidBody.velocity = new Vector3 (h * speed, r.y, r.z);
+	}
+
+	private void HandleVerticalBtn(){
+		float v = Input.GetAxis ("Vertical");
+		Vector3 r = myRigidBody.velocity;
+		myRigidBody.velocity = new Vector3 (r.x, v * speed, r.z);
 	}
 }
