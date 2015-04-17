e18ad5465a3431c94998d58fa9fd0bf17f9a4dd1
Allen Kashiwa <flyingcoding@gmail.com>
2015-04-17 22:12:12 +0800

    添加遮罩

---
+++ HearthStone/Assets/BlackMask.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlackMask : MonoBehaviour {
+
+	public static BlackMask _instance;
+	void Awake(){
+		_instance = this;
+		this.gameObject.SetActive (false);
+	}
+
+
+}
+
